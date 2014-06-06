6e39cc08b02527176e597d76468159a68256ad8b
Chris Elwood <kudzooman@gmail.com>
2014-06-05 20:34:16 -0700

    user profile unfinished

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,7 +1,15 @@
 class ArticlePolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
 
-def index?
-  true
-end
-
+  def index?
+    true
+  end
 end
