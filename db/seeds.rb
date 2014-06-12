34353ea25f64e4049f780810877de38c0b4abdb8
Ben Neely <benjaminneely@u.boisestate.edu>
2014-06-11 19:16:11 -0600

    Self is important

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,11 +21,41 @@ users = User.all
     )
 end
 
-5.times do
-  Contributor.create(
-    user: users.sample
-    )
-end
+# 5.times do
+#   Contributor.create(
+#     user: users.sample
+#     )
+# end
+
+
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save
+
+# Create a premium
+moderator = User.new(
+  name:     'Premium User',
+  email:    'premium@example.com', 
+  password: 'helloworld',
+  role:     'premium'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+)
+member.skip_confirmation!
+member.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
